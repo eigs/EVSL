3241faa617885f82315fb7f974a56cfb6ab8b83d
Ruipeng Li <liruipengblue@gmail.com>
2017-04-11 00:45:17 -0700

    minor change in the driver

--- EVSL_1.0/TESTS_Gen_Lap/LapPLanR.c
+++ EVSL_1.0/TESTS_Gen_Lap/LapPLanR.c
@@ -163,10 +163,10 @@ int main(int argc, char *argv[]) {
     set_pol_def(&pol);
     //-------------------- this is to show how you can reset some of the
     //                     parameters to determine the filter polynomial
-    pol.damping = 0;
+    pol.damping = 2;
     //-------------------- use a stricter requirement for polynomial
-    pol.thresh_int = 0.25;
-    pol.thresh_ext = 0.15;
+    pol.thresh_int = 0.8;
+    pol.thresh_ext = 0.5;
     pol.max_deg  = 300;
     // pol.deg = 20 //<< this will force this exact degree . not recommended
     //                   it is better to change the values of the thresholds
