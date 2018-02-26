7074e0e4e38846530f590ff64b2b0798a8d5e562
Ruipeng Li <li50@llnl.gov>
2018-02-26 14:01:49 -0800

    add polynomial approx. option to B-solve in a driver MMPLanN

--- EVSL_1.1.0/SRC/dos_utils.c
+++ EVSL_1.1.0/SRC/dos_utils.c
@@ -275,25 +275,3 @@ int lsPol(double (*ffun)(double), BSolDataPol *pol) {
   return 0;
 }
 
-#if 0
-/*
- * Recover the eigenvectors This is needed for GEN_MM folder only not DOS folder
- */
-int scalEigVec(int n, int nev, double *Y, double* sqrtdiag) {
-  // Formula (2.19) in the paper.  Y(:,i) is x in the paper and D^{1/2} is sqrtdiag here
-  // n: matrix size
-  // nev: number of eigenvectors V: n*nev
-  // V: computed eigenvectors
-  // sqrtdiag: square root of diagonal entries of B
-  int i, j;
-  double *v;
-  for (i=0; i<nev; i++){
-    v = &Y[i*n];
-    for (j=0; j<n; j++) {
-      v[j] = v[j]*sqrtdiag[j];
-    }
-  }
-  return 0;
-}
-#endif
-
